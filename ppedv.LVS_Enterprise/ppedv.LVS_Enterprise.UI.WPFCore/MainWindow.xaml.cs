3778d9615beb1dc98cc87f0d41b74747ae6d5ce8
André <ruland@outlook.de>
2020-05-13 12:29:22 +0200

    WPF UI fertig

--- ppedv.LVS_Enterprise/ppedv.LVS_Enterprise.UI.WPFCore/MainWindow.xaml.cs
+++ ppedv.LVS_Enterprise/ppedv.LVS_Enterprise.UI.WPFCore/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using ppedv.LVS_Enterprise.Model;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -29,9 +30,37 @@ namespace ppedv.LVS_Enterprise.UI.WPFCore
 
         LVSCore core = new LVSCore();
 
+        ObservableCollection<Artikel> artikeListe = null;
+
         private void Laden(object sender, RoutedEventArgs e)
         {
-            myGrid.ItemsSource = core.Repository.GetAll<Artikel>();
+            artikeListe = new ObservableCollection<Artikel>(core.Repository.GetAll<Artikel>());
+            myGrid.ItemsSource = artikeListe;
+        }
+
+        private void Speicher(object sender, RoutedEventArgs e)
+        {
+            core.Repository.Save();
+        }
+
+        private void Neu(object sender, RoutedEventArgs e)
+        {
+            var a = new Artikel() { Bezeichnung = "NEU" };
+            artikeListe.Add(a);
+            core.Repository.Add(a);
+        }
+
+        private void Delete(object sender, RoutedEventArgs e)
+        {
+            if (myGrid.SelectedItem is Artikel a)
+            {
+                if (MessageBox.Show($"Soll der Artikel {a.Bezeichnung} wirklich gelöscht werden?", "", MessageBoxButton.YesNo, MessageBoxImage.Warning)
+                    == MessageBoxResult.Yes)
+                {
+                    core.Repository.Delete(a);
+                    artikeListe.Remove(a);
+                }
+            }
         }
     }
 }
